2dc76cd39b1af47a069f9d260b3bd8b836ca3313
David <davidlechuga@ceroca.cat>
2023-09-18 18:48:26 +0200

    moviment vertical nau

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -17,12 +17,27 @@ public class NauJugador : MonoBehaviour
     {
         float direccioHorizontal = Input.GetAxisRaw("Horizontal");
         //Debug.Log(direccioHorizontal);
+        float direccioVertical = Input.GetAxisRaw("Vertical");
 
-        Vector2 direccioIndicada = new Vector2(direccioHorizontal, 0f);
+        Vector2 direccioIndicada = new Vector2(direccioHorizontal, direccioVertical).normalized;
+
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        float anchura = spriteRenderer.bounds.size.x / 2;
+        float largura = spriteRenderer.bounds.size.y / 2;
+
+        float limitEsquerraX = -Camera.main.orthographicSize * Camera.main.aspect + anchura;
+        float limitDretaX = Camera.main.orthographicSize * Camera.main.aspect - anchura;
+
+        float limitArribaY = Camera.main.orthographicSize -largura;
+        float limitAbajoY = -Camera.main.orthographicSize +largura;
 
         Vector2 novaPos = transform.position; //Ens retorna la posicio actual de la nau.
         novaPos += direccioIndicada * _velNau * Time.deltaTime;
 
+        novaPos.x = Mathf.Clamp(novaPos.x, limitEsquerraX, limitDretaX);
+        novaPos.y = Mathf.Clamp(novaPos.y, limitAbajoY, limitArribaY);
+
         transform.position = novaPos;
+
     }
 }
